afea3596746ce430d86de2785e67c7def1ffa705
Nasgun <nasgunawann@gmail.com>
2026-06-17 08:25:08 +0700

    fix: adjust postgres migration script to drop default before casting engine column

--- src/db/migrations/0003_slimy_pride.sql
+++ src/db/migrations/0003_slimy_pride.sql
@@ -1,1 +1,3 @@
-ALTER TABLE "vehicles" ALTER COLUMN "engine" SET DATA TYPE integer USING engine::integer;
+ALTER TABLE "vehicles" ALTER COLUMN "engine" DROP DEFAULT;
+ALTER TABLE "vehicles" ALTER COLUMN "engine" SET DATA TYPE integer USING (CASE WHEN engine = '' OR engine IS NULL THEN '0' ELSE engine END)::integer;
+ALTER TABLE "vehicles" ALTER COLUMN "engine" SET DEFAULT 0;
